cb2cbe19815c4fa13f26d0be6714a712b348c017
AleksT <aleks.tukeshev@mycelium.com>
2020-02-27 10:01:09 +0700

    Updates

---
+++ precache-manifest.7164a949f5c32b633bbbd741cf34285b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c87d974fec4fce4e58d2303d964451a",
+    "url": "/SiberKurt/store_vkmini.git/index.html"
+  },
+  {
+    "revision": "07cb84c26c51d4630efa",
+    "url": "/SiberKurt/store_vkmini.git/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "07cb84c26c51d4630efa",
+    "url": "/SiberKurt/store_vkmini.git/static/js/2.c3940c22.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/SiberKurt/store_vkmini.git/static/js/2.c3940c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75eb86714c3545bf64ff",
+    "url": "/SiberKurt/store_vkmini.git/static/js/main.8fd197c8.chunk.js"
+  },
+  {
+    "revision": "dc88b69837fb77bc9be0",
+    "url": "/SiberKurt/store_vkmini.git/static/js/runtime-main.dd4bea03.js"
+  }
+]);
